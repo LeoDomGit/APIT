a67173c04dddb50a0a0b6e414b619e63f3094016
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2022-12-03 08:12:26 +0700

    Commit

--- database/migrations/2022_12_03_010459_user_role.php
+++ database/migrations/2022_12_03_010459_user_role.php
@@ -13,19 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('userRole', function (Blueprint $table) {
             $table->integer('id',true,false);
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->string('password2')->nullable();
-            $table->text('ggId')->nullable();
             $table->boolean('status')->default(1);
-            $table->integer('idRole',false,false);
-            $table->rememberToken();
             $table->timestamps();
-            $table->foreign('idRole')->references('id')->on('userRole');
         });
     }
 
@@ -36,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('userRole');
     }
 };
