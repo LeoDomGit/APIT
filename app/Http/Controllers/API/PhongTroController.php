b0f465819ae8a1d204cb22148496b27c82e00c56
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2022-12-05 16:14:22 +0700

    C

--- app/Http/Controllers/API/PhongTroController.php
+++ app/Http/Controllers/API/PhongTroController.php
@@ -15,9 +15,9 @@ class PhongTroController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request,QuanM $QuanM,phongTroM $phongTroM)
     {
-        //
+        $result = DB::Table('roomtable')->join('quantable','roomtable.idQuan','quantable.id')->where('roomtable.idQuan','=',$request->id)->get('quantable.districtname as distrName',['*']);
     }
 
     /**
