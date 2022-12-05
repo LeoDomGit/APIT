6daa32d0af9774adfab157ef3e54e8638950caa5
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2022-12-05 07:55:31 +0700

    Commit

--- routes/api.php
+++ routes/api.php
@@ -24,27 +24,30 @@ use App\Http\Controllers\API\PhongTroController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware(['CheckRoute'])->group(function () {
+    Route::get('/themtk',[LoaiTKController::class,'themtk']);
+    Route::get('/allLTK',[LoaiTKController::class,'all']);
+    Route::post('/them',[LoaiTKController::class,'createthem']);
+    
+    // ==============================================
+    
+    Route::post('/addUser',[UserController::class,'create']);
+    Route::get('/allUser',[UserController::class,'all']);
+    Route::post('/switchUser',[UserController::class,'switch']);
+    Route::post('/checkUserLogin',[UserController::class,'checkLogin']);
+    // ==============================
+    
+    Route::post('/addQuan',[QuanController::class,'create']);
+    Route::GET('/allQuan',[QuanController::class,'all']);
+    Route::post('/editQuan',[QuanController::class,'edit']);
+    Route::post('/deleteQuan',[QuanController::class,'destroy']);
+    
+    // ==============================
+    Route::post('/addPhong',[PhongTroController::class,'create']);
+    Route::get('/allPhong',[PhongTroController::class,'all']);
+    Route::post('/deletePhong',[PhongTroController::class,'destroy']);
+    Route::post('/getSingleP',[PhongTroController::class,'single']);
+    Route::post('/editPhong',[PhongTroController::class,'edit']);
+    
+});
 
-Route::get('/themtk',[LoaiTKController::class,'themtk']);
-Route::get('/allLTK',[LoaiTKController::class,'all']);
-Route::post('/them',[LoaiTKController::class,'createthem']);
-
-// ==============================================
-
-Route::post('/addUser',[UserController::class,'create']);
-Route::get('/allUser',[UserController::class,'all']);
-Route::post('/switchUser',[UserController::class,'switch']);
-Route::post('/checkUserLogin',[UserController::class,'checkLogin']);
-// ==============================
-
-Route::post('/addQuan',[QuanController::class,'create']);
-Route::GET('/allQuan',[QuanController::class,'all']);
-Route::post('/editQuan',[QuanController::class,'edit']);
-Route::post('/deleteQuan',[QuanController::class,'destroy']);
-
-// ==============================
-Route::post('/addPhong',[PhongTroController::class,'create']);
-Route::get('/allPhong',[PhongTroController::class,'all']);
-Route::post('/deletePhong',[PhongTroController::class,'destroy']);
-Route::post('/getSingleP',[PhongTroController::class,'single']);
-Route::post('/editPhong',[PhongTroController::class,'edit']);
